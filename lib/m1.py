1a3870a410c3571a6d7b7692f0f8364e699f20cd
Ruben <ruben@ruben.com>
2018-02-20 19:56:04 +0100

    FInal del diaç

---
+++ lib/m1.py
@@ -0,0 +1,8 @@
+import sys
+def saludar(saludo):
+	print saludo
+
+def iniciales(nombre,ape1,ape2):          
+        iniciales=nombre[0]+'.'+ape1[0]+'.'+ape2[0]+'.'
+	print "Tus iniciales son:"+iniciales.upper()
+
