b920fcaff2fcf82f945823a992ca093e2c6a71d6
Ruben <ruben@ruben.com>
2018-02-21 15:44:16 +0100

    Hasta funciones

--- lib/m1.py
+++ lib/m1.py
@@ -1,8 +1,17 @@
-import sys
-def saludar(saludo):
-	print saludo
 
-def iniciales(nombre,ape1,ape2):          
-        iniciales=nombre[0]+'.'+ape1[0]+'.'+ape2[0]+'.'
-	print "Tus iniciales son:"+iniciales.upper()
+# -*- coding: utf-8 -*-
+
+def saludar(saludo):
+        print saludo
+
+def iniciales(nombre,ape1,ape2):
+        iniciales=nombre+[0]+'.'+ape1[0]+'.'+ape2[0]+'.'
+        return "Tus iniciales son:"+iniciales.upper()
+
+def iniciales1(nombre,ape1,apellidos):
+        iniciales=nombre[0]+'.'+ape1[0]
+        for ape in apellidos:
+                iniciales=iniciales+'.'+ape1[0]
+        return iniciales.upper()
+
 
