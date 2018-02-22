4fa1abce390472823582e6394149009da775d59b
Ruben <ruben@ruben.com>
2018-02-22 16:32:11 +0100

    Final Python con profesor

--- lib/ficheros.py
+++ lib/ficheros.py
@@ -32,9 +32,9 @@ def lista_tam(directorio,tam):
 	if tam[-1].upper() not in ('K','M','G'):
 		print 'Solo permite K,M,G,'
 		exit()
-	if tam[;-1]='K':
+	if tam[-1]=='K':
 		t=t*1024
-	if tam[:-1]=='M':
+	if tam[-1]=='M':
 		t=t*1024*1024
 	else:
 		t=t*1024*1024*1024
@@ -47,6 +47,69 @@ def lista_tam(directorio,tam):
 				print fichero + '--->' +str(os.path.getsize(f1))
 	
 
+#Unico fichero y que lo pinte.
+
+def cat(fichero):
+	if not os.access(fichero,0):
+		print 'FIchero no existe'
+		exit()
+	if not os.path.isfile(fichero):
+		print "No es un fichero"
+		exit()
+
+	f=open(fichero,'r')	
+	contenido=f.read()
+	print contenido
+	f.close()
+
+#UN grep, indicarle un fichero y un texto y que filtre por el.
 
 
-	 	
+def grep(fichero):
+
+	if not os.access(fichero,0):
+                print 'FIchero no existe'
+                exit()
+        if not os.path.isfile(fichero):
+                print "No es un fichero"
+                exit()
+	f=open(fichero,'r')
+	while True:
+		linea=f.readline()
+		if not linea:
+			break
+		if linea.count(texto)>0:
+			print linea[:-1]
+	
+def cp(fichero,destino):
+	if not os.access(fichero,0):
+                print 'FIchero no existe'
+                exit()
+        if not os.path.isfile(fichero):
+                print "No es un fichero"
+                exit()
+	if os.access(destino,0):
+                print 'FIchero ya existe'
+                exit()
+        if os.path.isfile(destino):
+                print "Es un archivo"
+                exit()
+	f1=open(fichero,'r')
+	f2=open(destino,'w')
+	lineas=f1.readlines()
+	for linea in lineas:
+		f2.write(linea)
+	f1.close()
+	f2.close()
+
+
+def ftp
+	from ftplib import ftp
+	ftp=FTP()
+	ftp.connect('192.168.1.54',23, -999)
+	ftp.login('oracle','oracle')
+#
+
+		
+
+
